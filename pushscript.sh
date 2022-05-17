ead0a847af73362bf6645b7c6966fce80806c327
benscruton <benjamin.scruton@gmail.com>
2022-05-16 21:35:51 -0500

    put words in newfile

--- pushscript.sh
+++ pushscript.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 git push origin main
-ssh foo@192.168.1.179 -p 8022 /bin/bash < "
+ssh foo@192.168.1.179 -p 8022 "/bin/bash
 	cd testing_remote_pushes
 	git pull origin main
 	echo 'Pulled latest update'
